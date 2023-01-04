d082e778e8079c1eb4855b2b4bad9c55596aecda
Fran Alarza <franalarza@gmail.com>
2023-01-04 01:00:50 +0100

    Añadir archivo dimens y login activity

--- app/src/main/java/com/franalarza/dragonballfights/LoginActivity.kt
+++ app/src/main/java/com/franalarza/dragonballfights/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.franalarza.dragonballfights
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        var color = ContextCompat.getColor(this, R.color.purple_200)
     }
 }
